7010d91341ab3b94d265df85b119a5040bde70c2
Pedro <71379301+locko980@users.noreply.github.com>
2024-03-15 11:35:09 -0300

    Update List_Filtering.py

--- List_Filtering.py
+++ List_Filtering.py
@@ -1,12 +1,3 @@
-# filter_list([1,2,'a','b']) == [1,2]
-# filter_list([1,'a','b',0,15]) == [1,0,15]
-# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
-
-
 def filter_list(l):
     int_list = [l[i] for i in range(len(l)) if type(l[i]) is int]
     return int_list
-
-print(filter_list([1,2,'a','b']))
-print(filter_list([1,'a','b',0,15]))
-print(filter_list([1,2,'aasf','1','123',123]))
